c2b6f3ce34254b7e5e92a487d8d38d5999beff01
Jordi Martí <jordimarticres@gmail.com>
2026-06-10 14:48:25 +0200

    corregido fichero original

--- matvec_membrana.py
+++ matvec_membrana.py
@@ -1,41 +1,34 @@
 import numpy as np
 import time
-import matplotlib.pyplot as plt
+from matplotlib import pyplot
 
 
 def creasis(N, mu):
     diagonal = np.ones(N * N) * (1 + 4 * mu)
     M = np.diag(diagonal)
-
-    vdi = -np.ones(N * N - 1) * mu
-
+    vdi = -np.ones(N * N - 1) * mu;
     for i in range(0, N * N - 1, N):
-        vdi[i] = 0
+        vdi[i] = 0;
 
     M = M + np.diag(vdi, 1) + np.diag(vdi, -1)
-
-    vdd = -np.ones(N * N - N) * mu
-    M = M + np.diag(vdd, -N) + np.diag(vdd, N)
-
-    return M
+    vdd = -np.ones(N * N - N) * mu;
+    M = M + np.diag(vdd, -N) + np.diag(vdd, N);
+    return (M)
 
 
 N = 20
 M = creasis(N, -1)
-
-plt.spy(M)
-plt.show()
-
+pyplot.spy(M)
+fil, col = M.shape
 b = np.random.rand(N * N, 1)
-
 start_time = time.perf_counter()
-
 for vuelta in range(50):
-    x0 = M @ b
+    x0 = np.zeros([N * N, 1])
+    for i in range(fil):
+        for j in range(col):
+            x0[i] = x0[i] + M[i, j] * b[j]
 
 end_time = time.perf_counter()
-
 print("Program finished in {} seconds".format(end_time - start_time))
-
-error = M @ b - x0
+error = (M @ b - x0)
 print('error =', np.linalg.norm(error))
